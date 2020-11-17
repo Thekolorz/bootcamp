4178b7ec1f3c79adc46d718189a8fb925ba0e649
Thekolorz <73794752+Thekolorz@users.noreply.github.com>
2020-11-17 18:07:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     new ScrollMagic.Scene({
       triggerElement: "#trigger1",
       triggerHook: 0.6, // show, when scrolled 10% into view
-      duration: "120%", // hide 10% before exiting view (80% + 10% from bottom)
+      duration: "250%", // hide 10% before exiting view (80% + 10% from bottom)
       offset: 50 // move trigger to center of element
     })
     .setClassToggle("#reveal1", "visible") // add class to reveal
