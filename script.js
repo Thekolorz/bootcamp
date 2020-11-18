22912e512729785f30ff5ac1146f6bd602944c9c
Thekolorz <73794752+Thekolorz@users.noreply.github.com>
2020-11-18 17:43:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
-  var controller = new ScrollMagic.Controller();
-  	// build scenes
-    new ScrollMagic.Scene({
-      triggerElement: "#trigger1",
-      triggerHook: 0.6, // show, when scrolled 10% into view
-      duration: "250%", // hide 10% before exiting view (80% + 10% from bottom)
-      offset: 50 // move trigger to center of element
+var controller = new ScrollMagic.Controller();
+// build scenes
+new ScrollMagic.Scene({
+        triggerElement: "#trigger1",
+        triggerHook: 0.6, // show, when scrolled 10% into view
+        duration: "250%", // hide 10% before exiting view (80% + 10% from bottom)
+        offset: 50 // move trigger to center of element
     })
     .setClassToggle("#reveal1", "visible") // add class to reveal
-/*     .addIndicators() // add indicators (requires plugin)
- */    .addTo(controller);
-
+    /*     .addIndicators() // add indicators (requires plugin)
+     */
+    .addTo(controller);
