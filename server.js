6e8b06970e889857a23255a7889d8805e3195ec6
Thekolorz <73794752+Thekolorz@users.noreply.github.com>
2021-01-05 15:24:31 +0100

    1

---
+++ server.js
@@ -0,0 +1,24 @@
+const Express = require('express');
+const Books = require('./Books')
+const app = Express();
+const port = 3000;
+app.use(Express.json());
+app.use(Express.urlencoded({extended: true}));
+function middleware(req, res, next) {
+    console.log(req.body);
+    console.log(req.query);
+    next();
+}
+// GET
+app.get("/books", middleware, (req, res) => {
+    res.json(Books);
+});
+app.get("/books/:id", (req, res) => {
+    let book = Books.find(book => book.id == req.params.id);
+    res.json(book);
+});
+// POST
+app.post("/testpost", (req, res) => {
+    console.log(req.body);
+})
+app.listen( port, () => console.log("HTTP server listening on port " + port))
